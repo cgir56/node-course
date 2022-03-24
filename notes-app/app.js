e7605134db8f2959eb1dc23eb5afecba30d7342a
cgir <cgir@hotmail.com>
2022-03-24 15:16:12 -0400

    First commit

---
+++ notes-app/app.js
@@ -0,0 +1,14 @@
+
+const getProut= require('./notes.js');
+// const fn = require('./notes.js');
+
+const msg = getProut();
+
+console.log(msg);
+
+
+
+// const fs=require('fs');
+
+// fs.writeFileSync('notes.txt', 'My name is Christian\n');
+// fs.appendFileSync('notes.txt', 'I live in Quebec\n');
