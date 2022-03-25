a7c9cefc14fac6d01a6de6b226f40d076e1c690f
cgir <cgir@hotmail.com>
2022-03-24 21:22:43 -0400

    training ending at yargs part 2

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # node-course
+code for the course ia at: https://github.com/andrewjmead/node-course-v3-code
--- notes-app/app.js
+++ notes-app/app.js
@@ -1,14 +1,73 @@
-
-const getProut= require('./notes.js');
-// const fn = require('./notes.js');
-
-const msg = getProut();
-
-console.log(msg);
+// loading modules
+const getNotes = require('./notes.js');
+const validator = require('validator');
+const chalk = require('chalk');
+const yargs = require('yargs');
 
 
+// define a shortcut for console.log
+const log = console.log;
 
-// const fs=require('fs');
+// testing validator
+// log(validator.isEmail('cgyr@hotmail.com'));
+// testing chalk
+// log (chalk.bold.red.inverse('Wow this is weird'));
 
-// fs.writeFileSync('notes.txt', 'My name is Christian\n');
-// fs.appendFileSync('notes.txt', 'I live in Quebec\n');
+
+// setup yargs
+
+yargs.version('1.0.1')
+
+// create the add command
+yargs.command({
+    command: 'add',
+    describe: 'add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'body of the note',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        log('Title: ' + argv.title)
+        log ('Body: ' + argv.body)
+    }
+})
+// create the read command
+yargs.command({
+    command: 'read',
+    describe: 'read a note',
+    handler: function(){
+        log('read a note')
+    }
+})
+// create the list command
+yargs.command({
+    command: 'list',
+    describe: 'list the notes',
+    handler: function(){
+        log('listing the notes')
+    }
+})
+// create the read command
+yargs.command({
+    command: 'read',
+    describe: 'read a note',
+    handler: function(){
+        log('reading a note')
+    }
+})
+
+log('parsing')
+
+
+
+// print yargs argv
+
+log(yargs.argv)
