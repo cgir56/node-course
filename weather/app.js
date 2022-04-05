7797f3a7c5ece2aa70f00cbdb2ca79458f4b74b7
cgirard <cgir@hotmail.com>
2022-04-04 23:57:15 -0400

    end of lesson 6_7

--- weather/app.js
+++ weather/app.js
@@ -1,16 +1,33 @@
 const request = require('request')
-const url = 'http://api.weatherstack.com/current?access_key=ed9ed73cf1637626337acd7e2877588a&query=45.479,-73.282'
-request({ url: url, json: true }, (_error, response) => {
-  console.log ('Temperature: ' + response.body.current.temperature)
-  console.log('Weather: ' + response.body.current.weather_descriptions)
-})
 
+// const url = 'http://api.weatherstack.com/current?access_key=ed9ed73cf1637626337acd7e2877588a&query=45.479,-73.282'
+// request({ url: url, json: true }, (error, response) => {
+//   if(error){
+//     console.log('Unable to connect to the weather service!')
+//   }
+//   else if (response.body.error) {
+//     console.log ('Unable to find location')
+//   }
+  
+//   else {
+//     console.log ('Temperature: ' + response.body.current.temperature)
+//     console.log ('Weather: ' + response.body.current.weather_descriptions)
+//   }
+  
+// })
 
-// new challenge of video 6_6
-const mapBoxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/1872%20rue%20des%20tulipes%20carigna%20quebec%20canada.json?types=address&access_token=pk.eyJ1IjoiY2dpcjU2IiwiYSI6ImNsMWpxOTYzYjIxaDIzanFpNmtvN24wOHoifQ.ALunGaboNNDkP3yBifilWw&limit=1'
-request({ url: mapBoxUrl, json: true }, (_error, response) => {
-  const longitude = response.body.features[0].center[1]
-  const latitude = response.body.features[0].center[0]
-  console.log('longitude: ' + longitude)
-  console.log('latitude: ' + latitude)
+// find the long/lat
+
+const mapBoxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/1872%20rue%20des%20tulipes%20carignan%20quebec%20canada.json?types=address&access_token=pk.eyJ1IjoiY2dpcjU2IiwiYSI6ImNsMWpxOTYzYjIxaDIzanFpNmtvN24wOHoifQ.ALunGaboNNDkP3yBifilWw&limit=1'
+request({ url: mapBoxUrl, json: true }, (error, response) => {
+  if (error) {
+    console.log('Unable to connect to the geocoding service!')
+  } else if (response.body.features.length < 1) {
+    console.log('Unable to find location!')
+  } else {
+    const longitude = response.body.features[0].center[1]
+    const latitude = response.body.features[0].center[0]
+    console.log('longitude: ' + longitude)
+    console.log('latitude: ' + latitude)
+  }
 })
