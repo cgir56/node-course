fcb821d53b2db49558ecdca6e0f00aa61f2aece5
cgir <cgir@hotmail.com>
2022-04-07 21:32:59 -0400

    end of lesson 6_9

--- weather/app.js
+++ weather/app.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
 
 // const url = 'http://api.weatherstack.com/current?access_key=ed9ed73cf1637626337acd7e2877588a&query=45.479,-73.282'
 // request({ url: url, json: true }, (error, response) => {
@@ -18,16 +19,24 @@ const request = require('request')
 
 // find the long/lat
 
-const mapBoxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/1872%20rue%20des%20tulipes%20carignan%20quebec%20canada.json?types=address&access_token=pk.eyJ1IjoiY2dpcjU2IiwiYSI6ImNsMWpxOTYzYjIxaDIzanFpNmtvN24wOHoifQ.ALunGaboNNDkP3yBifilWw&limit=1'
-request({ url: mapBoxUrl, json: true }, (error, response) => {
-  if (error) {
-    console.log('Unable to connect to the geocoding service!')
-  } else if (response.body.features.length < 1) {
-    console.log('Unable to find location!')
-  } else {
-    const longitude = response.body.features[0].center[1]
-    const latitude = response.body.features[0].center[0]
-    console.log('longitude: ' + longitude)
-    console.log('latitude: ' + latitude)
-  }
+// const mapBoxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/1872%20rue%20des%20tulipes%20carignan%20quebec%20canada.json?types=address&access_token=pk.eyJ1IjoiY2dpcjU2IiwiYSI6ImNsMWpxOTYzYjIxaDIzanFpNmtvN24wOHoifQ.ALunGaboNNDkP3yBifilWw&limit=1'
+// request({ url: mapBoxUrl, json: true }, (error, response) => {
+//   if (error) {
+//     console.log('Unable to connect to the geocoding service!')
+//   } else if (response.body.features.length < 1) {
+//     console.log('Unable to find location!')
+//   } else {
+//     const longitude = response.body.features[0].center[1]
+//     const latitude = response.body.features[0].center[0]
+//     console.log('longitude: ' + longitude)
+//     console.log('latitude: ' + latitude)
+//   }
+// })
+
+
+
+geocode('Boston', (error, data) =>{
+  console.log( 'Data: ', data )
+  console.log( 'Error: ', error)
+
 })
