e9272ab7744dad67e90e70b165aa4477f95c58c2
cgir <cgir@hotmail.com>
2022-04-13 21:33:40 -0400

    fixed return problem

--- weather/app.js
+++ weather/app.js
@@ -2,21 +2,24 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 // check the location argument
 const address = process.argv[2]
-if (address === undefined){
-  return console.log ('Enter a valid location')
+if (!address){
+  console.log ('Enter a valid location')
 }
-// chaining geocode and forecast
-geocode( address, (error, data) => {
-  if(error){
-    return console.log(error)
-  }
-  forecast(data.longitude, data.latitude, (error, forecastData) => {
+else{
+  // chaining geocode and forecast
+  geocode( address, (error, data) => {
     if(error){
       return console.log(error)
     }
-    console.log(data.location)
-    console.log(forecastData)
+    forecast(data.longitude, data.latitude, (error, forecastData) => {
+      if(error){
+        return console.log(error)
+      }
+      console.log(data.location)
+      console.log(forecastData)
+    })
   })
-  
-})
+}
+
+
 
