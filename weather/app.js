d33331c0690c90e9d6e3a69ab0c598e6bdf795f1
cgir <cgir@hotmail.com>
2022-04-13 21:25:30 -0400

    end of lesson 6_11

--- weather/app.js
+++ weather/app.js
@@ -1,41 +1,22 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
-
-
-// const url = 'http://api.weatherstack.com/current?access_key=ed9ed73cf1637626337acd7e2877588a&query=45.479,-73.282'
-// request({ url: url, json: true }, (error, response) => {
-//   if(error){
-//     console.log('Unable to connect to the weather service!')
-//   }
-//   else if (response.body.error) {
-//     console.log ('Unable to find location')
-//   }
-//   else {
-//     console.log ('Temperature: ' + response.body.current.temperature)
-//     console.log ('Weather: ' + response.body.current.weather_descriptions)
-//   }
-// })
-
-// find the long/lat
-
-//
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
-
-forecast(-543, 44.1545, (error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
+// check the location argument
+const address = process.argv[2]
+if (address === undefined){
+  return console.log ('Enter a valid location')
+}
+// chaining geocode and forecast
+geocode( address, (error, data) => {
+  if(error){
+    return console.log(error)
+  }
+  forecast(data.longitude, data.latitude, (error, forecastData) => {
+    if(error){
+      return console.log(error)
+    }
+    console.log(data.location)
+    console.log(forecastData)
+  })
+  
 })
 
-
-geocode('Boston', (error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
-})
