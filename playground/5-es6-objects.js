96cef171dd7a8a0fa9d67f87c2ff91519b3c28cb
cgirard <cgir@hotmail.com>
2022-04-16 11:59:45 -0400

    end of lesson 6_12

--- playground/5-es6-objects.js
+++ playground/5-es6-objects.js
@@ -8,3 +8,28 @@ const user = {
 }
 
 console.log(user)
+
+// object destructuring
+
+const product = {
+    label: 'red notebook',
+    price: 3,
+    stock : 201,
+    salePrice: undefined
+}
+
+// const label = product.label
+// const stock = product.stock
+
+// destructuring 
+// const { label:productLabel, stock, rating = 4.3} = product
+
+// console.log(productLabel)
+// console.log(stock)
+// console.log(rating)
+
+const transaction = (type, {label, stock}) => {     // label and stock are destructured from product
+    console.log(type, label, stock)
+}
+
+transaction('order', product)
