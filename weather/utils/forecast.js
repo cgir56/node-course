a48795706735bfa6e67a5b09c5419b063b38dea8
cgirard <cgir@hotmail.com>
2022-04-20 13:50:36 -0400

    end of lesson 13

--- weather/utils/forecast.js
+++ weather/utils/forecast.js
@@ -2,17 +2,14 @@ const request = require('request')
 
 const forecast = (longitude, latitude, callback) => {
   const url = 'http://api.weatherstack.com/current?access_key=ed9ed73cf1637626337acd7e2877588a&query=' + latitude + ',' + longitude
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, {body}) => {
     if(error){ 
       callback('Unable to connect to the weather service!',undefined)
-    }
-    else if (response.body.error) {
       callback('Unable to find location', undefined)
-    }
-    else {
+    } else {
       callback(undefined, {
-        temperature: response.body.current.temperature,
-        weather: response.body.current.weather_descriptions
+        temperature: body.current.temperature,
+        weather: body.current.weather_descriptions
       })
     }  
   })
